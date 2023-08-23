fc60fe411c9e2e54a116517e3876f0f41e74c184
druva <druva@netiapps.com>
2023-08-23 17:04:27 +0530

    Add crops page modified

--- resources/views/crops/add.blade.php
+++ resources/views/crops/add.blade.php
@@ -106,159 +106,27 @@
       <div class="row">
         <label class="label-bold">My Crops</label>
 
-       
-
-      <div class="row justify-content-centre">
-        @foreach($crops as $key=>$value)
-
-       
-        <div class="col-md-3 ">
-          <div class="card">
-            <div class="card-header text-align-centre">Channel - {{$value['channel_no']}} </div>
-            @if($value['name']!='')
-             <div class="row no-gutters" >
-              <div class="col-md-5">
-                <div class="" style="padding: 10px;">
-                   <img src="{{ URL::to('/') }}/crops/{{$value['image']}}" style="width: 100px;height: 100px;">
-                </div>
-                
-              </div>
-
-              <div class="col-md-7">
-                <div class="" style="padding-top: 10px">
-                   <label style="font-size: 17px;font-weight: bolder;">{{$value['name']}}</label>
-                   <div>
-                     <img src="{{ URL::to('/') }}/crops/stage.svg" style="width: 10px;height: 10px;">
-                     <label>{{$value['current_stage']}}</label>
-                   </div>
-
-                   <div>
-                     <img src="{{ URL::to('/') }}/crops/calendar.svg" style="width: 10px;height: 10px;">
-                     <label>Planted</label>
-                     <label>{{date('d M Y', strtotime($value['planted_date']))}}</label>
-                   </div>
-
-                   <div style="margin-top: 10px;margin-left: 2px" >
-                     <img src="{{ URL::to('/') }}/crops/info.svg" style="width: 10px;height: 10px;">
-                     <label style="font-size: 10px">Harvesting season in {{$value['harvesting_date']}} days</label>
-                     
-                   </div>
-                </div>
-                
-              </div>
-               
-             </div>
-
-             <div class="row">
-              <div class="col-md-6">
-                <a href="#"  id="MybtnModal_{{$key}}" data-id="{{$value['id']}}"><button class="btn btn-sm btn-outline-success" style="width: 100%">Edit</button></a>
-              </div>
-              <div class="col-md-6">
-                <a onclick="return confirm('The crop will be removed from this channel.')" href="{{route('remove_channel',$value['id'])}}"><button class="btn btn-sm btn-outline-danger" style="width: 100%">Remove</button></a>
-              </div>
-               
-               
-             </div>
-             @endif
-          </div>
-          </a>
+        <div class="row">
+           @foreach($crops as $key=>$value)
+           <div class="col">
+              <label>Chan{{$value['channel_no']}}</label>
+            @foreach($value['sub_chanel'] as $key2=>$value2)
+              @if($value2['id'] != '')
+               <label>{{$value2['name']}}</label>
+              @else
+               <label>No Crop</label>
+              @endif 
+            @endforeach 
+           </div>
+        
+        @endforeach
         </div>
 
-        <!-- Modal -->
-            <div class="modal" id="modal_{{$key}}" >
-              <div class="modal-dialog modal-xl">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Edit Channel - {{$value['channel_no']}}</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="modal-body">
-                    <div class="form-build">
-                      <div class="row">
-                        <div class="col-6">
-                        <form method="post" action="{{route('update_channel',$value['id'])}}">
-                          @method('PUT')
-                          @csrf
+       
 
-                           <div class="form-group row">
-                              <label for="" class="col-4 col-form-label">Channel no *</label>
-                              <div class="col-7">
-                                  <select class="form-select" name="channel_no" id="channel_no" required>
-                                     <option value="{{$value['channel_no']}}">Channel - {{$value['channel_no']}}</option>
-                                     @foreach($channel as $j)
-                                  
-                                     <option value="{{$j}}" >Channel - {{$j}}</option>
 
-                                     @endforeach
-                                    
-                                  </select>
-                              </div>
-                          </div>
 
-                          <div class="form-group row">
-                              <label for="" class="col-4 col-form-label">Crop Category *</label>
-                              <div class="col-7">
-                                  <select class="form-select" name="category" id="category_{{$key}}" required onchange="myFunction('{{$key}}')">
-                                    
-                                    @foreach($category as $key2=> $value2)
-
-                                     <option <?php echo ($value2->id == $value['category_id'])?'selected':''  ?> value="{{$value2->id}}">{{$value2->category_name}}</option>
-                                    
-                                    @endforeach
-                                  </select>
-                              </div>
-                          </div>
-
-                          <div class="form-group row">
-                              <label for="" class="col-4 col-form-label">Crop Name *</label>
-                              <div class="col-7" id="crop_{{$key}}">
-                                  <select class="form-control" name="crop">
-                                    <option value="{{$value['crop_id']}}" >{{$value['name']}}</option> 
-                                  </select>
-                                  
-                              </div>
-                          </div>
-
-                          <div class="form-group row">
-                              <label for="" class="col-4 col-form-label">Planted date *</label>
-                              <div class="col-7">
-                                   <input name="planted_on" id="planted_on" type="date" class="typeahead form-control" required="required" value="{{$value['planted_date']}}">
-                              </div>
-                          </div>
-
-                          <input type="hidden" name="pod_id" value="{{$id}}">
-                          
-                          <div class="form-group row">
-                              <label for="" class="col-4 col-form-label"></label>
-                              <div class="col-7">
-                                  <button class="btn btn-sm btn-outline-success" type="Submit">Submit</button>
-                              </div>
-                          </div>
-
-                        </form>
-
-                        </div>
-                          
-                      </div>
-                        
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          <!--  end Modal -->
-           <script>
-          $(document).ready(function(){
-            $('#MybtnModal_{{$key}}').click(function(){
-              $('#modal_{{$key}}').modal('show');
-            });
-          });  
-          </script>
-          
-
-         
-        @endforeach
-      </div>   
+        
     </div>
         
   </div>
